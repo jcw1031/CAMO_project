fe3f240f0903a80e95cce6268503035ac4ca3a97
woopaca <jcw001031@gmail.com>
2022-11-19 19:49:07 +0900

    로그인 기능 추가

--- CAMO_App/CAMO/Api/SignupApi.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignupApi.swift
-//  CAMO
-//
-//  Created by 지찬우 on 2022/11/07.
-//
-
-import Foundation
--- camoServer/src/main/java/jcw/camoServer/controller/UserController.java
+++ camoServer/src/main/java/jcw/camoServer/controller/UserController.java
@@ -1,5 +1,6 @@
 package jcw.camoServer.controller;
 
+import jcw.camoServer.dto.LoginDto;
 import jcw.camoServer.entity.User;
 import jcw.camoServer.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,7 @@ import java.util.Optional;
 @Slf4j
 @RestController
 @RequestMapping("/user")
-public class MemberController {
+public class UserController {
 
     @Autowired
     UserService userService;
@@ -24,15 +25,43 @@ public class MemberController {
     public User signUp(@RequestBody User user) {
         log.info("user = {}", user);
         User joinUser = userService.join(user);
+        joinUser.setRole(0);
         log.info("user = {}", joinUser);
         System.out.println(user == joinUser);
         return joinUser;
     }
 
     /**
+     * 로그인
+     */
+    @PostMapping("/login")
+    public User login(@RequestBody LoginDto loginDto) {
+        System.out.println("호출");
+        log.info("loginDto = {}", loginDto);
+        Optional<User> user = userService.findByEmail(loginDto.getEmail());
+        if (user.isPresent() && (user.get().getPassword().equals(loginDto.getPassword()))) {
+            return user.get();
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/login/{email}")
+    public User loginGet(@PathVariable("email") String email, @RequestParam("password") String password) {
+        System.out.println("호출");
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            return user.get();
+        } else {
+            System.out.println("예외 처리");
+            return null;
+        }
+    }
+
+    /**
      * 모든 회원 조회
      */
-    @GetMapping("/search/all")
+    @GetMapping("/list")
     public List<User> userList() {
         return userService.findAll();
     }
