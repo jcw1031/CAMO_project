922a227001db84b9c2114aabc5153e729349d040
woopaca <jcw001031@gmail.com>
2022-11-21 02:14:51 +0900

    apply .gitignore

--- CAMO_Spring/src/main/java/jcw/CAMO_Spring/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package jcw.CAMO_Spring.repository;
-
-import jcw.CAMO_Spring.domain.Coupon;
-
-public interface CouponRepository {
-    Coupon add(Coupon Coupon);
-}
--- CAMO_Spring/src/main/java/jcw/CAMO_Spring/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package jcw.CAMO_Spring.repository;
-
-import jcw.CAMO_Spring.domain.Review;
-
-public interface ReviewRepository {
-
-    Review save(Review review);
-}
