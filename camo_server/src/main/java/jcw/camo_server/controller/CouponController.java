e2aeb56255906a60d3acf9300a8bc5ed76f8dea5
woopaca <jcw001031@gmail.com>
2022-12-07 14:22:46 +0900

    :sparkles: coupon 사용 추가

--- camo_server/src/main/java/jcw/camo_server/controller/CouponController.java
+++ camo_server/src/main/java/jcw/camo_server/controller/CouponController.java
@@ -28,14 +28,24 @@ public class CouponController {
         couponService.giveCoupon(couponDto);
     }
 
+    /**
+     * 사용자별 쿠폰 리스트
+     * @param userId 쿠폰 리스트를 불러올 userId
+     * @return userId를 가진 사용자의 쿠폰 리스트
+     */
     @GetMapping("/user/{id}")
     public List<CouponListDTO> userCouponList(@PathVariable("id") Long userId) {
         return couponService.userCouponList(userId);
     }
 
+    /**
+     * 쿠폰 사용
+     * @param cafeId 카페의 cafeId
+     * @param userId 사용자의 userId
+     */
     @PutMapping("/use")
-    public void couponUse() {
-
+    public void couponUse(@RequestParam("cafeId") String cafeId, @RequestParam("userId") Long userId) {
+        couponService.useCoupon(cafeId, userId);
     }
 
     @GetMapping("/list")
--- camo_server/src/main/java/jcw/camo_server/service/CouponService.java
+++ camo_server/src/main/java/jcw/camo_server/service/CouponService.java
@@ -2,7 +2,9 @@ package jcw.camo_server.service;
 
 import jcw.camo_server.dto.coupon.CouponDTO;
 import jcw.camo_server.dto.coupon.CouponListDTO;
+import jcw.camo_server.entity.Cafe;
 import jcw.camo_server.entity.Coupon;
+import jcw.camo_server.mapper.CafeMapper;
 import jcw.camo_server.mapper.CouponMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,7 @@ import java.util.Optional;
 public class CouponService {
 
     private final CouponMapper couponMapper;
+    private final CafeMapper cafeMapper;
 
     @Transactional
     public void giveCoupon(CouponDTO couponDto) {
@@ -41,6 +44,26 @@ public class CouponService {
     @Transactional
     public List<CouponListDTO> userCouponList(Long userId) {
         return couponMapper.findByUser(userId);
+    }
+
+    public void useCoupon(String cafeId, Long userId) {
+        Optional<Coupon> optionalCoupon = couponMapper.findCoupon(userId, cafeId);
+        Optional<Cafe> optionalCafe = cafeMapper.findById(cafeId);
+        if (optionalCoupon.isPresent() && optionalCafe.isPresent()) {
+            Coupon coupon = optionalCoupon.get();
+            Cafe cafe = optionalCafe.get();
+
+            if (coupon.getCouponUserstamp() >= cafe.getCafeRewardstamp()) {
+                couponMapper.updateCoupon(Coupon.builder()
+                        .couponId(coupon.getCouponId())
+                        .couponUserstamp(coupon.getCouponUserstamp() - cafe.getCafeRewardstamp())
+                        .build());
+            } else {
+                throw new IllegalArgumentException("쿠폰이 부족합니다!");
+            }
+        } else {
+            throw new IllegalArgumentException("잘못된 요청입니다!");
+        }
     }
 
     public List<Coupon> couponList() {
